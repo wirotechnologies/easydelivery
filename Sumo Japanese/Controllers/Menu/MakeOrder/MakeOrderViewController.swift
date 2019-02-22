d128ac92cade0308474f8d6c6fd22a26a9eafbad
Diego Paredes <diegorobh@gmail.com>
2019-02-21 20:47:09 -0500

    trabajando en MakeOrder

---
+++ Sumo Japanese/Controllers/Menu/MakeOrder/MakeOrderViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MakeOrderViewController.swift
+//  Sumo Japanese
+//
+//  Created by imac on 21/02/19.
+//  Copyright © 2019 Wiro Technologies. All rights reserved.
+//
+
+import UIKit
+
+class MakeOrderViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
